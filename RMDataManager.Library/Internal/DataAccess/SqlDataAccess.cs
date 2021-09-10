fde847f7c2608ea11a4740f3456a172a7e2d802b
himanshuparmar121 <parmarhimanshu360@gmail.com>
2021-09-10 23:25:06 +0530

    Wired up the api with Database to get Id

---
+++ RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDataManager.Library.Internal.DataAccess
+{
+    public class SqlDataAccess
+    {
+        public string GetConnectionString(String name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+
+        public List<T> LoadData<T, U>(string storedProcedures, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                List<T> rows = connection.Query<T>(storedProcedures, parameters, 
+                    commandType: CommandType.StoredProcedure).ToList();
+                
+                return rows;
+            }
+        }
+
+        public void SaveData<T, U>(string storedProcedures, U parameters, string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            using (IDbConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Execute(storedProcedures, parameters,
+                    commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
